6f0608d82b723a5b651ab199b36333b44815a172
Kanhaiyalaljoshi <kanhaiyalaljoshi626@gmail.com>
2025-09-01 22:51:20 +0530

    Update bot.py

--- bot.py
+++ bot.py
@@ -1,1 +1,19 @@
-python bot.py
+from pyrogram import Client
+import os
+
+API_ID = os.getenv("API_ID")
+API_HASH = os.getenv("API_HASH")
+BOT_TOKEN = os.getenv("BOT_TOKEN")
+
+bot = Client(
+    "my_bot",
+    api_id=API_ID,
+    api_hash=API_HASH,
+    bot_token=BOT_TOKEN
+)
+
+@bot.on_message()
+def start(client, message):
+    message.reply("Hello! Bot is working 🚀")
+
+bot.run()
