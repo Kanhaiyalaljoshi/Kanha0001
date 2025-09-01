b53be2fed836449be53e9243ea00035aed8af1f5
Kanhaiyalaljoshi <kanhaiyalaljoshi626@gmail.com>
2025-09-01 22:58:22 +0530

    Update and rename requirements.txt to config.py

---
+++ config.py
@@ -0,0 +1,5 @@
+
+API_ID = int(os.getenv("API_ID", "12345"))
+API_HASH = os.getenv("API_HASH", "your_api_hash")
+BOT_TOKEN = os.getenv("BOT_TOKEN", "your_bot_token")
+OWNER_ID = int(os.getenv("OWNER_ID", "123456789"))  # अपना Telegram User
